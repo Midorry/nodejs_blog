16cf4dc6f4c2f0a62474358f0606f2a2ec22d2ff
Midorry <nguyenvantrantam23052002@gmail.com>
2023-11-01 22:29:42 +0700

    [MVC] Routes & Controller

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// HTTP logger
+// app.use(morgan("combined"));
+
+// Route init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
